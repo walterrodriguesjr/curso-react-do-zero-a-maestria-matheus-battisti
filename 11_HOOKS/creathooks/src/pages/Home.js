2792307664300422d68b327c4c2b404e91897cfa
Walter <you@example.com>
2023-02-24 13:19:18 -0300

    atualizando

--- 11_HOOKS/creathooks/src/pages/Home.js
+++ 11_HOOKS/creathooks/src/pages/Home.js
@@ -1,11 +1,14 @@
 import React from 'react'
+import HookUseReducer from '../components/HookUseReducer'
 import HookUseState from '../components/HookUseState'
+
 
 const Home = () => {
   return (
     <div>
         HookUseState
         <HookUseState />
+        <HookUseReducer />
     </div>
   )
 }
