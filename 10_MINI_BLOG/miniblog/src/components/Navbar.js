4f7f9876f28c3b9d2fed173092024e809530e0a8
Walter <you@example.com>
2023-03-02 13:29:27 -0300

    atualizando

--- 10_MINI_BLOG/miniblog/src/components/Navbar.js
+++ 10_MINI_BLOG/miniblog/src/components/Navbar.js
@@ -6,16 +6,22 @@ import { NavLink } from 'react-router-dom';
 
 const Navbar = () => {
     return (
-        <nav>
-            <NavLink to="/">
+        <nav className={styles.navbar}>
+            <NavLink to="/" className={styles.brand}>
                 Mini <span>Blog</span>
             </NavLink>
-            <ul>
+            <ul className={styles.links_list}>
                 <li>
-                    <NavLink to="/">Home</NavLink>
+                    <NavLink to="/" className={({ isActive }) => (isActive ? styles.active : "")}>Home</NavLink>
                 </li>
                 <li>
-                    <NavLink to="/about">Sobre</NavLink>
+                    <NavLink to="/login" className={({ isActive }) => (isActive ? styles.active : "")}>Login</NavLink>
+                </li>
+                <li>
+                    <NavLink to="/register" className={({ isActive }) => (isActive ? styles.active : "")}>Register</NavLink>
+                </li>
+                <li>
+                    <NavLink to="/about" className={({ isActive }) => (isActive ? styles.active : "")}>Sobre</NavLink>
                 </li>
             </ul>
         </nav>
---
+++ 10_MINI_BLOG/miniblog/src/pages/Login/Login.js
@@ -0,0 +1,12 @@
+//CSS
+import styles from './Login.module.css';
+
+const Login = () => {
+  return (
+    <div>
+        <h1>Login</h1>
+    </div>
+  )
+}
+
+export default Login
